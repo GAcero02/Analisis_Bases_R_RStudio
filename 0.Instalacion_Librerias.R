64922edd1f99bfeb24d08f73734f2760fa7b3071
Gerhard Acero <119520363+GAcero02@users.noreply.github.com>
2026-06-17 06:24:19 -0500

    Add files via upload

---
+++ 0.Instalacion_Librerias.R
@@ -0,0 +1,13 @@
+#se instalan los paquetes necesarios para este proceso
+
+#tidyverse es una colección de paquetes de R diseñada para importar, limpiar, 
+#transformar, visualizar y analizar datos de manera organizada y consistente
+install.packages(tidyverse)
+
+#janitor es un paquete de R que facilita la limpieza y organización de bases de datos 
+#ademása estandariza nombres, explora frecuencias y prepara datos para el análisis
+install.packages(janitor)
+
+#skimr es un paquete de R que genera resúmenes exploratorios rápidos y detallados 
+#de un conjunto de datos, mostrando estadísticas descriptivas según el tipo de variable
+install.packages(skimr)
